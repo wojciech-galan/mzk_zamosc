6b4d824f61d9144375c4b4e4ccbda8a29189074e
wojtek <wojciech-galan@gmail.com>
2022-05-17 14:55:24 +0200

    now file is importable from outside the project

--- mzk_zamosc/lib/StopPageParsers.py
+++ mzk_zamosc/lib/StopPageParsers.py
@@ -1,3 +1,5 @@
+import os
+import sys
 
 import bs4.element
 import requests
@@ -5,14 +7,17 @@ from functools import partial
 from bs4 import BeautifulSoup
 from typing import List
 
-from dtos import A
-from dtos import Departures
-from functions import tag_has_attribute
-from functions import is_aligned
-from functions import has_only_child_with_attribute
+PROJECT_DIR = os.path.abspath(__file__).rsplit(os.path.sep, 3)[0]
+sys.path.insert(0, PROJECT_DIR)
+
+from mzk_zamosc.lib.dtos import A
+from mzk_zamosc.lib.dtos import Departures
+from mzk_zamosc.lib.functions import tag_has_attribute
+from mzk_zamosc.lib.functions import is_aligned
+from mzk_zamosc.lib.functions import has_only_child_with_attribute
 
 
-def get_lines_from_given_stop(line_url:str) -> List[A]:
+def get_lines_from_given_stop(line_url: str) -> List[A]:
     req = requests.get(line_url)
     soup = BeautifulSoup(req.content, 'html.parser')
     table: bs4.element.Tag = soup.body.table
@@ -20,25 +25,27 @@ def get_lines_from_given_stop(line_url:str) -> List[A]:
     return hrefs[:-1]
 
 
-def parse_stop_page(stop_url:str):
+def parse_stop_page(stop_url: str):
     req = requests.get(stop_url)
     soup = BeautifulSoup(req.content, 'html.parser')
     tr: bs4.element.Tag = soup.body.table.tr
-    found:bs4.element.ResultSet[bs4.element.Tag] = tr.findAll(tag_has_bgcolor)
+    found: bs4.element.ResultSet[bs4.element.Tag] = tr.findAll(tag_has_bgcolor)
     assert len(found) == 1
     timetable = found[0]
-    #print(timetable.prettify())
+    # print(timetable.prettify())
     for tr in timetable.findAll(align=is_center_aligned):
-        #print(tr.prettify())
+        # print(tr.prettify())
         if has_one_td_with_class(tr):
             print(td_has_class_is_followed_by_td_without_class(tr))
-    #print(found[0].prettify())
+    # print(found[0].prettify())
 
 
 def td_has_class_is_followed_by_td_without_class(tr: bs4.element.Tag) -> List[bs4.element.Tag]:
-    if has_one_td_with_class(tr) and not has_one_td_with_class(tr.next_sibling) and not has_one_td_with_class(tr.next_sibling.next_sibling):
+    if has_one_td_with_class(tr) and not has_one_td_with_class(tr.next_sibling) and not has_one_td_with_class(
+            tr.next_sibling.next_sibling):
         # print(tr.next_sibling.next_sibling.prettify())
-        return Departures(parse_departure_days(tr), parse_departure_hours(tr.next_sibling), parse_departure_hours(tr.next_sibling.next_sibling))
+        return Departures(parse_departure_days(tr), parse_departure_hours(tr.next_sibling),
+                          parse_departure_hours(tr.next_sibling.next_sibling))
 
 
 def parse_departure_days(tr: bs4.element.Tag) -> str:
@@ -61,9 +68,8 @@ tag_has_bgcolor = partial(tag_has_attribute, attribute_name='bgcolor')
 is_center_aligned = partial(is_aligned, alignment="CENTER")
 has_one_td_with_class = partial(has_only_child_with_attribute, child_name='td', attribute_name='class')
 
-
 if __name__ == '__main__':
-    parse_stop_page('http://www.mzk.zamosc.pl/pliki/rozklad/0056/0056t030.htm')
+    # parse_stop_page('http://www.mzk.zamosc.pl/pliki/rozklad/0056/0056t030.htm')
     print(get_lines_from_given_stop('http://www.mzk.zamosc.pl/pliki/rozklad/p/p0120.htm'))
     # import urllib.request
     # #with urllib.request.urlopen('http://www.mzk.zamosc.pl/pliki/rozklad/p/p0120.htm') as handle:
